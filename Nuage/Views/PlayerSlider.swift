ff5a919a102c2cc9e6ed689d86194840a46dbbc9
Laurin Brandner <mail@laurinbrandner.ch>
2021-02-03 11:55:13 +0100

    Working player sliders

--- Nuage/Views/PlayerSlider.swift
+++ Nuage/Views/PlayerSlider.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct PlayerSlider<Value : BinaryFloatingPoint>: View {
     
     @Binding private var value: Value
+    @State private var updatingValue: Value?
+    @GestureState private var highlighted = false
     private var range: ClosedRange<Value>
     private var continuousUpdate: Bool
     
@@ -17,9 +19,26 @@ struct PlayerSlider<Value : BinaryFloatingPoint>: View {
         let knobRadius: CGFloat = 13
         
         GeometryReader { geometry in
-            let relativeValue = range.upperBound > 0 ? CGFloat(value/range.upperBound) : CGFloat(0)
+            let currentValue = updatingValue ?? value
+            let rangeWidth = range.upperBound - range.lowerBound
+            let relativeValue = rangeWidth > 0 ? CGFloat(currentValue/rangeWidth) : CGFloat(0)
             let barValue = geometry.size.width * relativeValue
             let knobValue = (geometry.size.width - knobRadius) * relativeValue
+            let knobColor = highlighted ? Color(NSColor.tertiaryLabelColor) : Color(NSColor.windowBackgroundColor)
+            
+            let drag = DragGesture(minimumDistance: 0, coordinateSpace: .local).onChanged { gesture in
+                var newValue = Value(gesture.location.x/geometry.size.width) * rangeWidth + range.lowerBound
+                newValue = min(max(newValue, range.lowerBound), range.upperBound)
+                
+                if continuousUpdate { value = newValue }
+                else { updatingValue = newValue }
+            }.onEnded { gesture in
+                value = updatingValue ?? value
+                updatingValue = nil
+            }
+            .updating($highlighted) { _, highlighted, _ in
+                highlighted = true
+            }
             
             ZStack(alignment: Alignment(horizontal: .leading, vertical: .center)) {
                 HStack {
@@ -34,10 +53,12 @@ struct PlayerSlider<Value : BinaryFloatingPoint>: View {
                 .frame(height: 3)
                 Circle()
                     .strokeBorder(Color(NSColor.tertiaryLabelColor), lineWidth: 1)
-                    .background(Circle().foregroundColor(Color(NSColor.windowBackgroundColor)))
+                    .background(Circle().foregroundColor(knobColor))
                     .frame(width: knobRadius, height: knobRadius)
                     .offset(x: knobValue)
             }
+            .contentShape(Rectangle())
+            .gesture(drag)
         }.frame(height: knobRadius)
     }
 
