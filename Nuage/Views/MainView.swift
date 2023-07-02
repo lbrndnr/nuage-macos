4b789645d15e3d1a6f93e7eb17a10544c22d6bd2
Laurin Brandner <mail@laurinbrandner.ch>
2023-07-02 11:20:28 +0200

    Fix preview, cleanup

--- Nuage/Views/MainView.swift
+++ Nuage/Views/MainView.swift
@@ -25,6 +25,50 @@ struct MainView: View {
     @EnvironmentObject private var commands: Commands
     
     var body: some View {
+        return VStack(spacing: 0) {
+//            let presentSearch = Binding(get: { searchQuery.count > 0 },
+//                                        set: { presented in
+//                                            if !presented {
+//                                                searchQuery = ""
+//                                            }
+//                                        })
+            let stackItem: Binding<Int?> = Binding(get: { presentProfile ? 1 : (searchQuery.count > 0 ? 2 : nil) },
+                                                      set: { value in
+                                                        if value == nil {
+                                                            presentProfile = false
+                                                            searchQuery = ""
+                                                        }
+                                                      })
+            
+            StackNavigationView(selection: $navigationSelection, content: sidebar)
+            .stack(item: stackItem) {
+                if presentProfile {
+                    UserDetail(user: SoundCloud.shared.user!)
+                }
+                else {
+                    let search = SoundCloud.shared.get(.search(searchQuery))
+                    SearchList(for: search)
+                }
+            }
+            PlayerView()
+        }
+        .frame(minWidth: 800, minHeight: 400)
+        .toolbar(content: toolbar)
+        .onAppear {            
+            SoundCloud.shared.get(.library())
+                .map { $0.collection }
+                .replaceError(with: [])
+                .receive(on: RunLoop.main)
+                .sink { likes in
+                    let playlists = likes.map { $0.item }
+                    self.playlists = playlists
+                    SoundCloud.shared.user?.playlists = playlists
+                }
+                .store(in: &self.subscriptions)
+        }
+    }
+    
+    @ViewBuilder private func sidebar() -> some View {
         let stream = SoundCloud.shared.get(.stream())
         let streamView = PostList(for: stream).navigationTitle("Stream")
         
@@ -41,97 +85,29 @@ struct MainView: View {
             .eraseToAnyPublisher()
         let followingView = UserGrid(for: following).navigationTitle("Following")
         
-        return VStack(spacing: 0) {
-//            let presentSearch = Binding(get: { searchQuery.count > 0 },
-//                                        set: { presented in
-//                                            if !presented {
-//                                                searchQuery = ""
-//                                            }
-//                                        })
-            let stackItem: Binding<Int?> = Binding(get: { presentProfile ? 1 : (searchQuery.count > 0 ? 2 : nil) },
-                                                      set: { value in
-                                                        if value == nil {
-                                                            presentProfile = false
-                                                            searchQuery = ""
-                                                        }
-                                                      })
-            
-            StackNavigationView(selection: $navigationSelection) {
-                List {
-                    sidebarNavigationLink(title: "Stream", imageName: "bolt.horizontal.fill", destination: streamView, tag: 0)
-                    Section(header: Text("Library")) {
-                        sidebarNavigationLink(title: "Likes", imageName: "heart.fill", destination: likesView, tag: 1)
-                        sidebarNavigationLink(title: "History", imageName: "clock.fill", destination: historyView, tag: 2)
-                        sidebarNavigationLink(title: "Following", imageName: "person.2.fill", destination: followingView, tag: 3)
+        List {
+            sidebarNavigationLink(title: "Stream", imageName: "bolt.horizontal.fill", destination: streamView, tag: 0)
+            Section(header: Text("Library")) {
+                sidebarNavigationLink(title: "Likes", imageName: "heart.fill", destination: likesView, tag: 1)
+                sidebarNavigationLink(title: "History", imageName: "clock.fill", destination: historyView, tag: 2)
+                sidebarNavigationLink(title: "Following", imageName: "person.2.fill", destination: followingView, tag: 3)
+            }
+            Section(header: Text("Playlists")) {
+                ForEach(0..<playlists.count, id: \.self) { idx in
+                    let playlist = playlists[idx]
+                    let ids = SoundCloud.shared.get(.playlist(playlist.id))
+                        .map { $0.trackIDs ?? [] }
+                        .eraseToAnyPublisher()
+                    let slice = { ids in
+                        return SoundCloud.shared.get(.tracks(ids))
                     }
-                    Section(header: Text("Playlists")) {
-                        ForEach(0..<playlists.count, id: \.self) { idx in
-                            let playlist = playlists[idx]
-                            let ids = SoundCloud.shared.get(.playlist(playlist.id))
-                                .map { $0.trackIDs ?? [] }
-                                .eraseToAnyPublisher()
-                            let slice = { ids in
-                                return SoundCloud.shared.get(.tracks(ids))
-                            }
 
-                            let playlistView = TrackList(for: ids, slice: slice).navigationTitle(playlist.title)
-                            SidebarNavigationLink(playlist.title, destination: playlistView, tag: idx+4, selection: $navigationSelection)
-                        }
-                    }
+                    let playlistView = TrackList(for: ids, slice: slice).navigationTitle(playlist.title)
+                    SidebarNavigationLink(playlist.title, destination: playlistView, tag: idx+4, selection: $navigationSelection)
                 }
-                .listStyle(SidebarListStyle())
-            }
-            .stack(item: stackItem) {
-                if presentProfile {
-                    UserDetail(user: SoundCloud.shared.user!)
-                }
-                else {
-                    let search = SoundCloud.shared.get(.search(searchQuery))
-                    SearchList(for: search)
-                }
-            }
-            PlayerView()
-        }
-        .frame(minWidth: 800, minHeight: 400)
-        .toolbar {
-            ToolbarItem {
-                TextField("􀊫 Search", text: $searchQuery)
-                    .onExitCommand { searchQuery = "" }
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .frame(minWidth: 150)
-
-            }
-//            ToolbarItem {
-//                MenuButton(label: Image(systemName: "arrow.up.arrow.down")) {
-//                    Text("Recently Added")
-//
-//                }
-//            }
-            ToolbarItem {
-                Button(action: { presentProfile = true }) {
-                    HStack {
-                        Text(SoundCloud.shared.user?.username ?? "Profile")
-                            .bold()
-                            .foregroundColor(.secondary)
-                        RemoteImage(url: SoundCloud.shared.user?.avatarURL, cornerRadius: 15)
-                            .frame(width: 30, height: 30)
-                    }
-                }
-                .buttonStyle(PlainButtonStyle())
             }
         }
-        .onAppear {            
-            SoundCloud.shared.get(.library())
-                .map { $0.collection }
-                .replaceError(with: [])
-                .receive(on: RunLoop.main)
-                .sink { likes in
-                    let playlists = likes.map { $0.item }
-                    self.playlists = playlists
-                    SoundCloud.shared.user?.playlists = playlists
-                }
-                .store(in: &self.subscriptions)
-        }
+        .listStyle(SidebarListStyle())
     }
     
     @ViewBuilder private func sidebarNavigationLink<V: View>(title: String, imageName: String, destination: V, tag: Int) -> some View {
@@ -144,10 +120,45 @@ struct MainView: View {
         }
     }
     
+    @ToolbarContentBuilder private func toolbar() -> some ToolbarContent {
+        ToolbarItem {
+            TextField("􀊫 Search", text: $searchQuery)
+                .onExitCommand { searchQuery = "" }
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(minWidth: 150)
+
+        }
+//            ToolbarItem {
+//                MenuButton(label: Image(systemName: "arrow.up.arrow.down")) {
+//                    Text("Recently Added")
+//
+//                }
+//            }
+        ToolbarItem {
+            Button(action: { presentProfile = true }) {
+                HStack {
+                    Text(SoundCloud.shared.user?.username ?? "Profile")
+                        .bold()
+                        .foregroundColor(.secondary)
+                    RemoteImage(url: SoundCloud.shared.user?.avatarURL, cornerRadius: 15)
+                        .frame(width: 30, height: 30)
+                }
+            }
+            .buttonStyle(PlainButtonStyle())
+        }
+    }
+    
 }
 
-//struct MainView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        MainView()
-//    }
-//}
+struct MainView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        let player = StreamPlayer()
+        player.enqueue(Preview.tracks)
+        
+        return MainView()
+            .environmentObject(player)
+            .environmentObject(Commands())
+    }
+    
+}
