d1f7553e19626ffbd0b7fc5ebff6b8104e3fcf76
Laurin Brandner <mail@laurinbrandner.ch>
2024-03-21 08:39:42 +0100

    Bump version

--- Nuage/Views/InfiniteView/PageView.swift
+++ Nuage/Views/InfiniteView/PageView.swift
@@ -16,10 +16,10 @@ private let initialPageSubscription = 0
 struct PageView<Element: Decodable&Identifiable, ContentView: View>: View {
     
     var elements: [Element] {
-        return pages.map { $0.collection }
-            .reduce([], +)
+        return pages?.map { $0.collection }
+            .reduce([], +) ?? []
     }
-    @State private var pages = [Page<Element>]()
+    @State private var pages: [Page<Element>]?
     
     private var publisher: AnyPublisher<Page<Element>, Error>
     @State private var subscriptions = [Int: AnyCancellable]()
@@ -28,19 +28,30 @@ struct PageView<Element: Decodable&Identifiable, ContentView: View>: View {
     
     var body: some View {
         Group {
-            if elements.isEmpty {
+            if let pages = pages {
+                if pages.isEmpty {
+                    Text("Empty")
+                        .font(.title2)
+                        .foregroundColor(.secondary)
+                }
+                else {
+                    content(elements, getNextPage)
+                }
+            }
+            else {
                 ProgressView()
                     .progressViewStyle(.circular)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
-            else {
-                content(elements, getNextPage)
-            }
         }
         .onAppear {
             publisher.receive(on: RunLoop.main)
-                .sink(receiveCompletion: { _ in }, receiveValue: { page in
-                    pages.append(page)
+                .sink(receiveCompletion: { completion in
+                    if case .failure(_) = completion {
+                        pages = []
+                    }
+                }, receiveValue: { page in
+                    pages = [page]
                 })
                 .store(in: &subscriptions, key: initialPageSubscription)
         }
@@ -55,16 +66,16 @@ struct PageView<Element: Decodable&Identifiable, ContentView: View>: View {
     private func getNextPage() {
         guard subscriptions[elements.count] == nil else { return }
 
-        let currentPagePublisher = pages.publisher
+        let currentPagePublisher = (pages ?? []).publisher
             .last()
-            .mapError{ $0 as Error }
+            .mapError { $0 as Error }
 
         publisher.merge(with: currentPagePublisher)
             .first()
             .flatMap { SoundCloud.shared.get(next: $0) }
             .receive(on: RunLoop.main)
             .sink(receiveCompletion: { _ in }, receiveValue: { page in
-                pages.append(page)
+                pages?.append(page)
             })
             .store(in: &subscriptions, key: elements.count)
     }
